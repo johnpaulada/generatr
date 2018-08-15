aa12146a35c890ad8179ea372ab2e6243d7c773d
John Paul Ada <johnpaulada@gmail.com>
2018-08-15 12:50:39 +0800

    Initial Commit

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify-es';
+
+export default {
+  input: 'src/Generatr.js',
+  output: [
+    {
+      file: 'index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'index.es.js',
+      format: 'es'
+    },
+    {
+      file: 'generatr.min.js',
+      format: 'iife',
+      name: 'Generatr'
+    }
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    uglify()
+  ]
+};
